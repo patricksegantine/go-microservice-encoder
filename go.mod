c5e3a3c6be850cc6a6fd973db6aa6e992c82fb5a
Patrick Segantine <patricksegantine@gmail.com>
2022-01-10 09:02:56 -0300

    added gorm and infrastructure layer

--- go.mod
+++ go.mod
@@ -5,8 +5,16 @@ go 1.17
 require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
 	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/jinzhu/gorm v1.9.16
+	github.com/lib/pq v1.1.1
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/satori/go.uuid v1.2.0
 	github.com/stretchr/testify v1.7.0
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/mattn/go-sqlite3 v1.14.0 // indirect
 )
---
+++ infrastructure/database/db.go
@@ -0,0 +1,67 @@
+package database
+
+import (
+	"encoder/internal/domain"
+	"log"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/lib/pq"
+)
+
+type Database struct {
+	Db            *gorm.DB
+	Dsn           string
+	DbType        string
+	Debug         bool
+	AutoMigrateDb bool
+	Env           string
+}
+
+func NewDb() *Database {
+	return &Database{}
+}
+
+func NewDbTest() *gorm.DB {
+	dbInstance := NewDb()
+	dbInstance.Env = "Test"
+	dbInstance.DbType = "sqlite3"
+	dbInstance.Dsn = ":memory:"
+	dbInstance.AutoMigrateDb = true
+	dbInstance.Debug = true
+
+	connection, err := dbInstance.Connect()
+
+	if err != nil {
+		log.Fatalf("Test db error: %v", err)
+	}
+
+	return connection
+}
+
+func (d *Database) Connect() (*gorm.DB, error) {
+
+	var err error
+	d.Db, err = gorm.Open(d.DbType, d.Dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if d.Debug {
+		d.Db.LogMode(true)
+	}
+
+	if d.AutoMigrateDb {
+		d.Db.AutoMigrate(&domain.Video{}, &domain.Job{})
+
+		d.Db.Model(domain.Job{}).AddForeignKey(
+			"video_id",
+			"videos (id)",
+			"CASCADE",
+			"CASCADE",
+		)
+	}
+
+	return d.Db, nil
+}
