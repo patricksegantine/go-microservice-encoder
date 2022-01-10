c861adf28ca2a0e19c61bfaffff1ce568c0b24bf
Patrick Segantine <patricksegantine@gmail.com>
2022-01-10 06:01:01 -0300

    added validation for job and tests

--- internal/domain/job.go
+++ internal/domain/job.go
@@ -1,13 +1,50 @@
 package domain
 
-import "time"
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
 
 type Job struct {
-	ID               string
-	OutputBucketPath string
-	Status           string
-	Video            *Video
-	Error            string
-	CreatedAt        time.Time
-	UpdatedAt        time.Time
+	ID               string    `valid:"uuid"`
+	OutputBucketPath string    `valid:"notnull"`
+	Status           string    `valid:"notnull"`
+	Video            *Video    `valid:"-"`
+	VideoID          string    `valid:"-"`
+	Error            string    `valid:"-"`
+	CreatedAt        time.Time `valid:"-"`
+	UpdatedAt        time.Time `valid:"-"`
+}
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+func NewJob(output string, status string, video *Video) (*Job, error) {
+	job := Job{
+		OutputBucketPath: output,
+		Status:           status,
+		Video:            video,
+	}
+	job.prepare()
+
+	_, err := job.Validate()
+
+	if err != nil {
+		return nil, err
+	}
+	return &job, nil
+}
+
+func (job *Job) prepare() {
+	job.ID = uuid.NewV4().String()
+	job.CreatedAt = time.Now()
+}
+
+// Validate return will be equal to `false` if there are any errors
+func (job *Job) Validate() (bool, error) {
+	result, err := govalidator.ValidateStruct(job)
+	return result, err
 }
